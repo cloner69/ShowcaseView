a0356600752dabbdae34cb34989cbbd2839b4b2f
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-03 16:36:39 +0700

    Showcase view

---
+++ ShowcaseView/ShowcaseView/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ShowcaseView
+//
+//  Created by Adrian Suryo Abiyoga on 21/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShowcaseView/ShowcaseView/ShowcaseViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowcaseViewApp.swift
+//  ShowcaseView
+//
+//  Created by Adrian Suryo Abiyoga on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ShowcaseViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
